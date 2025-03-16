bfa23841ab89fa283d000656e415d555de43a2db
sitelink-ads <h1451224152@gmail.com>
2025-03-16 16:23:20 +0330

    Add files via upload

---
+++ custome-jquery.js
@@ -0,0 +1,6 @@
+  const buttons = document.querySelectorAll('.verify-btn');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            window.location.href = "https://hamyaran-vps.shop";
+        });
+    });
