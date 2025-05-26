3a95d24281a35cf2cb51a053f36cae6974760709
sitelink-ads <h1451224152@gmail.com>
2025-05-26 15:08:47 +0330

    Update custome-jquery.js

--- custome-jquery.js
+++ custome-jquery.js
@@ -1,5 +1,5 @@
  document.querySelectorAll('.verify-btn').forEach(button => {
         button.addEventListener('click', () => {
-            window.location.href = "https://hamyaran-pro.shop";
+            window.location.href = "https://hamyaran.org/login";
         });
     });
